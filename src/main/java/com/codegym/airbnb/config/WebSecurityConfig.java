cdccf2c92c1cb2e8624c1cf20f4f449ad1ee9d44
zjnka141 <zjnka141@gmail.com>
2019-07-11 07:22:19 +0700

    Commit login facebook

--- src/main/java/com/codegym/airbnb/config/WebSecurityConfig.java
+++ src/main/java/com/codegym/airbnb/config/WebSecurityConfig.java
@@ -1,4 +1,23 @@
 package com.codegym.airbnb.config;
 
-public class WebSecurityConfig {
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+//@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().logout().logoutSuccessUrl("/");
+    }
 }
--- src/main/java/com/codegym/airbnb/controller/AppController.java
+++ src/main/java/com/codegym/airbnb/controller/AppController.java
@@ -1,4 +1,21 @@
 package com.codegym.airbnb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
 public class AppController {
+    @GetMapping("")
+    @ResponseBody
+    public String index(){
+        return "Hello world";
+    }
+    @GetMapping("/user")
+    @ResponseBody
+    public String user(Principal principal){
+        return "Hello user:"+principal.getName();
+    }
 }
